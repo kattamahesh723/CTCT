c14fe96807c18141b54a23d0a7be77e0a2274396
Mahesh Katta <M4h3sh@Maheshs-MacBook-Pro.local>
2017-12-25 11:29:20 +0530

    Create array and construct tree are added into helper file

--- Trees/TreeHelper.cpp
+++ Trees/TreeHelper.cpp
@@ -1,5 +1,22 @@
 #include "TreeHelper.h"
 
+node* createArrayAndReturnTree(){
+	int size;
+	cout<<"Enter the size of the array"<<endl;
+	cin>>size;
+
+	int *array = (int *)malloc(sizeof(int)*size);
+
+	cout<<"Enter elements into array"<<endl;
+
+	for(int i=0; i<size; i++)
+		cin>>array[i];
+
+	node *root = constructTree(array, 0, size-1);
+
+	return root;
+}
+
 node* constructTree(int *array, int start, int end){
 
 	if(start > end)
---
+++ Trees/treeTest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "TreeHelper.h"
+using namespace std;
+
+int main()
+{
+	node *root = createArrayAndReturnTree();
+
+	cout<<"In order traversal"<<endl;
+	inOrder(root);
+	cout<<endl;
+
+	cout<<"Pre order traversal"<<endl;
+	preOrder(root);
+	cout<<endl;
+
+	cout<<"Post order traversal"<<endl;
+	postOrder(root);
+	cout<<endl;
+}
